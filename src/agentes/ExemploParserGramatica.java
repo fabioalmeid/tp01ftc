9edf4e304e9bdaca6628a532f24d4f3c41118bdd
fabioalmeid <fabio_desenv@yahoo.com.br>
2013-09-07 17:13:23 -0400

    melhorando exemplo de parser da gramatica

--- src/agentes/ExemploParserGramatica.java
+++ src/agentes/ExemploParserGramatica.java
@@ -12,8 +12,8 @@ import java.io.InputStreamReader;
 
 public class ExemploParserGramatica {
 	public static void main(String args[]) throws Exception {
-		String str = "Iniciar Medicao Temperatura";
-		//String str = "Iniciar Monitoramento Temperatura";
+		//String str = "Iniciar Medicao Temperatura";
+		String str = "Iniciar Monitoramento Temperatura";
 		 
 		// convert String into InputStream
 		InputStream is = new ByteArrayInputStream(str.getBytes());
@@ -26,8 +26,8 @@ public class ExemploParserGramatica {
 		try {
 			parse_tree = p.pTarefa();
 		} catch (Error e) {
-			System.out.println("String '" + str + "' nao pertence a linguagem.");
-			throw new Exception("String '" + str + "' pertence a linguagem.");
+			//System.out.println("String '" + str + "' nao pertence a linguagem.");
+			throw new Exception("String '" + str + "' nao pertence a linguagem. Erro: " + e.getMessage());
 		}
 
 		System.out.println(PrettyPrinter.print(parse_tree));
