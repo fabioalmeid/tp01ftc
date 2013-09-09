24738e5fe2106c4c37415b2bf774edcea0e81940
Fabio Moreira <fabiomoreira@nhemu.com>
2013-09-09 13:46:36 -0400

    agentes se publicam nas paginas amarelas, adicionado parser com objeto

--- src/agentes/AgenteMonitor.java
+++ src/agentes/AgenteMonitor.java
@@ -1,31 +1,54 @@
 package agentes;
 
+import gramatica.Centralizador.Absyn.ECollect1;
+import gramatica.Centralizador.Absyn.ECollect2;
+import gramatica.Centralizador.Absyn.EData1;
+import gramatica.Centralizador.Absyn.EData2;
+import gramatica.Centralizador.Absyn.EData3;
+import gramatica.Centralizador.Absyn.EData4;
 import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.List;
+import java.util.Random;
 
-import util.Actions;
-import util.ParseGrammar;
+import agentes.jade.Centralizador.GrammarParserCentralizador;
+import agentes.jade.Centralizador.TarefaCentralizador;
 
 public class AgenteMonitor extends Agent {
 
 	private static final long serialVersionUID = 1L;
 
-	private String currentAction;
-
-	private int currentDegree, currentIndex;
-
 	private ACLMessage ACLmsg;
+	private Boolean isTempRunning = false;
+	private Behaviour medeTemperatura = null; 
 
 	protected void setup() {
+		
+		// Regista o monitor no servico de paginas amarelas
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.setName(getAID());
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("monitor");
+		sd.setName(getName());
+		dfd.addServices(sd);
+		try {
+			DFService.register(this, dfd);
+		} catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
 
 		addBehaviour(new CyclicBehaviour(this) {
-
 			private static final long serialVersionUID = 1L;
 
 			@Override
@@ -34,23 +57,74 @@ public class AgenteMonitor extends Agent {
 				ACLmsg = receive(MessageTemplate.MatchPerformative(ACLMessage.REQUEST));
 
 				if (ACLmsg != null) {
-
-					if (ParseGrammar.validateSentence(ACLmsg.getContent().toString())) { // GRAMMAR validation
-						System.out.println("ParseGramma  TRUE");
-
-						if (ACLmsg.getContent().toString().contains(Actions.INICIAR_LEITURA_TEMPERATURA_MONITOR)) {
-							addBehaviour(new InformTempBehaviour(myAgent,ACLmsg));
+					String mensagem = ACLmsg.getContent().toString();
+					try { 
+						TarefaCentralizador tc = GrammarParserCentralizador.getCentralizadorMessageObject(mensagem);
+						if (tc.getAcao() instanceof ECollect1) { // veja na gramatica o que significa ECollect1
+							System.out.print("Monitor: Iniciando a medicacao");
+							List<Object> dados = tc.getDados();
+							for (Object o : dados){
+								if (o instanceof EData1) {
+									System.out.print(" Temperatura\n");
+									if (!isTempRunning) {
+										addBehaviour(new InformTempBehaviour(myAgent,ACLmsg));
+										isTempRunning = true;
+									} else System.out.println("Monitor " + getName() + " ja esta monitorando temperatura.");
+								}
+								else if (o instanceof EData2)
+									System.out.print(" Hemoglobina\n");
+								else if (o instanceof EData3)
+									System.out.print(" bilirrubina\n");
+								else if (o instanceof EData4)
+									System.out.print(" Pressao Arterial\n");
+							}
 						}
-						// OTHERS
-
-					} else {
-						System.out.println("FALSE");
+						else if (tc.getAcao() instanceof ECollect2) {
+							System.out.print("Monitor: Parando medicao");
+							List<Object> dados = tc.getDados();
+							for (Object o : dados){
+								if (o instanceof EData1) {
+									System.out.print(" Temperatura\n");
+									if (isTempRunning) {
+										removeBehaviour(medeTemperatura);
+									} else System.out.println("\nMonitor " + getName() + " nao esta monitorando temperatura.");
+								}
+								else if (o instanceof EData2)
+									System.out.print(" Hemoglobina\n");
+								else if (o instanceof EData3)
+									System.out.print(" bilirrubina\n");
+								else if (o instanceof EData4)
+									System.out.print(" Pressao Arterial\n");
+							}
+							
+						}/*
+						else if (tc.getAcao() instanceof EApply1) {
+							System.out.print("Liberando medicacao");
+							List<Medicamento> med = tc.getMedicacao();
+							ImprimeMedicacao(med);
+						}
+						else if (tc.getAcao() instanceof EApply2){
+							System.out.print("Cessando liberacao da medicacao");
+							List<Medicamento> med = tc.getMedicacao();
+							ImprimeMedicacao(med);
+							}
+						System.out.println("\n-------------------");
+						*/	
+					} catch (Exception e){
+						System.out.println(e.getMessage());
 					}
-
+//					if (ParseGrammar.validateSentence(ACLmsg.getContent().toString())) { // GRAMMAR validation
+//						System.out.println("ParseGramma  TRUE");
+//
+//						if (ACLmsg.getContent().toString().contains(Actions.INICIAR_LEITURA_TEMPERATURA_MONITOR)) {
+//							addBehaviour(new InformTempBehaviour(myAgent,ACLmsg));
+//						}
+//						// OTHERS
+//					} else {
+//						System.out.println("FALSE");
+//					}
 				}
-
 			}
-
 		});
 	}
 
@@ -61,7 +135,7 @@ public class AgenteMonitor extends Agent {
 //
 //		msgtoSend = "A " + action + "16:00: "+ Integer.toString(currentNumber);
 
-		System.out.println("msg enviada vy monitor " + msgToSend );
+		//System.out.println("msg enviada vy monitor " + msgToSend );
 		replica.setContent(msgToSend);
 		send(replica);
 
@@ -71,15 +145,15 @@ public class AgenteMonitor extends Agent {
 		return (int) (Math.random() * max);
 	}
 	
-	private void generateTempMSG(ACLMessage ACLmsg,int previousDegree){
-		String 	msgtoSend  ;
-		int currentDegree = randomNumber(previousDegree);
-		
-		msgtoSend =  "A " + "TEMPERATURA"+ " as "+  getCurrentTime() + ": "+ Integer.toString(currentDegree) + "C    " + " |" + Integer.toString(currentDegree) ;  // DEFINIR MELHOR A ACTION(TEMPERATURA) E horario(random)
-		System.out.println("msgtoSend  >>>>> "  +  msgtoSend);
-		sendMsgtoCentralizador(ACLmsg, msgtoSend);
-		
-	}
+//	private void generateTempMSG(ACLMessage ACLmsg,int previousDegree){
+//		String 	msgtoSend  ;
+//		int currentDegree = randomNumber(previousDegree);
+//		
+//		msgtoSend =  "A " + "TEMPERATURA"+ " as "+  getCurrentTime() + ": "+ Integer.toString(currentDegree) + "C    " + " |" + Integer.toString(currentDegree) ;  // DEFINIR MELHOR A ACTION(TEMPERATURA) E horario(random)
+//		System.out.println("msgtoSend  >>>>> "  +  msgtoSend);
+//		sendMsgtoCentralizador(ACLmsg, msgtoSend);
+//		
+//	}
 	
 	private String getCurrentTime(){
 	
@@ -94,25 +168,37 @@ public class AgenteMonitor extends Agent {
 		sendMsgtoCentralizador(ACLmsg, msgtoSend);
 		
 	}
+	
+	
+		
 
 	
 	// MONITOR informs to Centralizador a given temperature at each 2s
 	class InformTempBehaviour extends TickerBehaviour {
 		private static final long serialVersionUID = 1L;
-
 		private ACLMessage aclMessage;
 
 		public InformTempBehaviour(Agent a, ACLMessage aclMessage) {
 			super(a, 2000);
 			this.aclMessage = aclMessage;
-			System.out.println("aclMessage********************************"+ aclMessage);
+			System.out.println("Monitor: aclMessage********************************"+ aclMessage);
 		}
 
 		@Override
 		protected void onTick() {
-
-			generateTempMSG(aclMessage, 40);  // get CurrentDegree
-
+			//generateTempMSG(aclMessage, 40);  // get CurrentDegree
+//			int currentDegree = randomNumber(previousDegree);
+//			
+//			msgtoSend =  "A " + "TEMPERATURA"+ " as "+  getCurrentTime() + ": "+ Integer.toString(currentDegree) + "C    " + " |" + Integer.toString(currentDegree) ;  // DEFINIR MELHOR A ACTION(TEMPERATURA) E horario(random)
+//			System.out.println("msgtoSend  >>>>> "  +  msgtoSend);
+//			sendMsgtoCentralizador(ACLmsg, msgtoSend);
+			Random rn = new Random();
+			int Min = 36;
+			int Max = 41;
+			int temperatura = Min + (int)(Math.random() * ((Max - Min) + 1));
+			String resposta = "Temperatura de " + temperatura + " C as 19 h: 59 m";
+			System.out.println("Monitor: msgtoSend para Centralizador  >>>>> "  +  resposta);
+			sendMsgtoCentralizador(aclMessage, resposta);
 		}
 	}
 	
--- src/agentes/jade/Centralizador/Centralizador.java
+++ src/agentes/jade/Centralizador/Centralizador.java
@@ -17,26 +17,23 @@ import jade.wrapper.StaleProxyException;
 import java.util.ArrayList;
 import java.util.List;
 
+import util.GeradorAleatorioMsg;
 import agentes.jade.Monitor.Afericao;
 import agentes.jade.Monitor.GrammarParserMonitor;
 import agentes.jade.Monitor.TarefaMonitor;
-
-import util.Actions;
-import util.GeradorAleatorioMsg;
 
 public class Centralizador extends Agent {
 	private static final int INICIO_FEBRE = 38;
 	private static final int INTERVALO_REQUISICAO = 6000;
 	private final int AGENTSNUMBER = 2;
 	
+	private List<AID> monitor;
+	private List<AID> atuador;
+	
 	private String monitorName, atuadorName;
 	
-	// Lista de todos os monitores e atuadores
-	private List<AID> monitores = new ArrayList<AID>();
-	private List<AID> atuadores = new ArrayList<AID>();
-
 	public void setup() {
-		System.out.println("Sou o agente monitor " + getAID().getName() + " e estou pronto.");
+		System.out.println("Centralizador: Sou o agente monitor " + getAID().getName() + " e estou pronto.");
 
 		// create agent t1 on the same container of the creator agent
 		AgentContainer container = (AgentContainer) getContainerController();
@@ -44,11 +41,11 @@ public class Centralizador extends Agent {
 		for (int i = 0; i < AGENTSNUMBER; i++) {
 			// create MONITOR
 			monitorName = "monitor" + Integer.toString(i);
-			monitores.add(CreatNewAgent(container, monitorName, "agentes.AgenteMonitor"));
+			CreatNewAgent(container, monitorName, "agentes.AgenteMonitor");
 
 			// create ATUADOR
 			atuadorName = "atuador" + Integer.toString(i);
-			atuadores.add(CreatNewAgent(container, atuadorName, "agentes.AgenteAtuador"));
+			CreatNewAgent(container, atuadorName, "agentes.AgenteAtuador");
 		}
 
 		// minute
@@ -56,7 +53,45 @@ public class Centralizador extends Agent {
 			protected void onTick() {
 				// manda para algum monitor uma mensagem aleatoria
 				int indexMsg = (int) (Math.random() * (AGENTSNUMBER-1));
-				sendMsgToMonitor(GeradorAleatorioMsg.getRandomMessageCentralizador(), "monitor" + indexMsg);
+				sendMessageToAgent(GeradorAleatorioMsg.getRandomMessageCentralizador(), "monitor" + indexMsg);
+				
+				//atualiza de tempos em tempos lista de agentes
+				// Atualiza lista de monitores
+				DFAgentDescription template = new DFAgentDescription();
+				ServiceDescription sd = new ServiceDescription();
+				sd.setType("monitor");
+				template.addServices(sd);
+				try {
+					DFAgentDescription[] result = DFService.search(myAgent, template); 
+					System.out.print("Centralizador: Achei os seguintes monitores:");
+					monitor = new ArrayList<AID>(); 
+					for (int i = 0; i < result.length; ++i) {
+						monitor.add(result[i].getName());
+						System.out.print(" | " + monitor.get(i).getName());
+					}
+					System.out.println();
+				}
+				catch (FIPAException fe) {
+					fe.printStackTrace();
+				}
+				
+				// Atualiza lista de atuadores
+//				DFAgentDescription template2 = new DFAgentDescription();
+//				ServiceDescription sd2 = new ServiceDescription();
+//				sd2.setType("atuador");
+//				template2.addServices(sd2);
+//				try {
+//					DFAgentDescription[] result2 = DFService.search(myAgent, template2); 
+//					System.out.println("Achei os seguintes atuadores:");
+//					atuador = new ArrayList<AID>();
+//					for (int i = 0; i < result2.length; ++i) {
+//						atuador.add(result2[i].getName());
+//						System.out.println(atuador.get(i).getName());
+//					}
+//				}
+//				catch (FIPAException fe) {
+//					fe.printStackTrace();
+//				}
 			}
 		});
 		
@@ -71,8 +106,8 @@ public class Centralizador extends Agent {
 					AID sender = msg.getSender();
 
 					// se o sender for monitor
-					if (monitores.contains(sender)) {
-						System.out.println("Msg recebida do monitor " + msg.getSender().getLocalName() + " -->" + msg.getContent());
+					if (monitor.contains(sender)) {
+						System.out.println("Centralizador: Msg recebida do monitor " + msg.getSender().getLocalName() + " -->" + msg.getContent());
 						// valida mensagem
 						TarefaMonitor tm = new TarefaMonitor();
 						try {
@@ -82,21 +117,17 @@ public class Centralizador extends Agent {
 							for (Afericao af : listaAF) {
 								if (af.getDado() instanceof EDados) { // veja gramatica para EDados
 									if (af.getQuantidade1() > INICIO_FEBRE)
-										System.out.print("Esta com febre, devo aplicar remedio");										
+										System.out.print("Centralizador: " + sender.getName() + " esta com febre, devo aplicar remedio");
+									else System.out.print("Centralizador: " + sender.getName() + " tem temperatura boa, nao precisa remedio");
 								}
 							}
 						} catch (Exception e){
 							System.out.println(e.getMessage());
 						}
 					}
-					else if (atuadores.contains(sender))
-						System.out.println("Msg recebida do atuador " + msg.getSender().getLocalName() + " -->" + msg.getContent());
-					else System.out.println("ERRO: Mensagem recebida do desconhecido");
-
-//					defineAction(Integer.valueOf(msg.getContent().substring(
-//							index, msg.getContent().toString().length())), msg
-//							.getSender().getLocalName());
-
+					else if (atuador.contains(sender))
+						System.out.println("Centralizador: Msg recebida do atuador " + msg.getSender().getLocalName() + " -->" + msg.getContent());
+					else System.out.println("Centralizador: ERRO: Mensagem recebida do desconhecido");
 					System.out.println("**********************");
 					System.out.println();
 				}
@@ -105,80 +136,23 @@ public class Centralizador extends Agent {
 		});
 	}
 
-//	@SuppressWarnings("null")
-//	private void defineAction(int CurrentTemp, String agentName){
-//		switch (CurrentTemp) {
-//		case 33:
-//			System.out.println("**************************");
-//			System.out.println("3333333");
-//			System.out.println("**************************");
-//			sendMsgToMonitor(Actions.PARAR_LEITURA_TEMPERATURA_MONITOR, (Integer) null, agentName );
-//			break;
-//
-//		default:
-//			break;
-//		}		
-//	}
 	private AID CreatNewAgent(AgentContainer container, String agentName, String agentAdrress) {
 		AID minhaID = null;
 		try {
 			System.out.println("agentName  ++++" + agentName);
 			container.createNewAgent(agentName, agentAdrress, null).start();
-
-			// Regista o novo agente no servico de paginas amarelas
-			DFAgentDescription dfd = new DFAgentDescription();
-			minhaID = getAID();
-			dfd.setName(minhaID);
-			ServiceDescription sd = new ServiceDescription();
-			sd.setType(agentAdrress);
-			sd.setName(agentName);
-			dfd.addServices(sd);
-			DFService.register(this, dfd);
-
 		} catch (StaleProxyException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (FIPAException fe) {
-			fe.printStackTrace();
 		}
 		return minhaID;
 	}
 
-	private String defineMonitorAction(int switchCaseLenght) {
-		int indexMsg = (int) (Math.random() * switchCaseLenght);
-		String msgSelected = null;
-		switch (indexMsg) {
-		case 0:
-			msgSelected = Actions.INICIAR_LEITURA_TEMPERATURA_MONITOR;
-			break;
-		case 1:
-			msgSelected = Actions.INICIAR_LEITURA_TEMPERATURA_MONITOR;
-//			msgSelected = Actions.INICIAR_LEITURA_HEMOGRAMA_MONITOR; //////// just test, RETURN to default
-			break;
-
-		}
-		return msgSelected;
-
-	}
-
-	private void sendMsgToMonitor(String action, String agentName) {
+	private void sendMessageToAgent(String mensagem, String agentName) {
 		ACLMessage message = new ACLMessage(ACLMessage.REQUEST);
 		message.addReceiver(new AID(agentName, AID.ISLOCALNAME));
-		String currentMsg = defineMonitorAction(2);
-//		message.setContent( currentMsg + "_" + currentValue);
-		message.setContent(currentMsg);
-		System.out.println(agentName + " " + currentMsg);
-		// System.out.println("To enviando uma mensagem...");
-		send(message);
-	}
-
-	private void sendMsgToAtuador() {
-		ACLMessage message = new ACLMessage(ACLMessage.REQUEST);
-		message.addReceiver(new AID(atuadorName, AID.ISLOCALNAME));
-		message.setContent("faz alguma coisa ai ATUADOR!!!");
-
-		System.out.println(atuadorName + "faz alguma coisa ai ATUADOR!!!");
-		// System.out.println("To enviando uma mensagem...");
+		message.setContent(mensagem);
+		System.out.println("Centralizador: Enviei mensagem para " + agentName + " : " + mensagem);
 		send(message);
 	}
 }
