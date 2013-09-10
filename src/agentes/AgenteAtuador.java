e3805493d0993367250aa9275e074ab59329998c
fabioalmeid <fabio_desenv@yahoo.com.br>
2013-09-09 22:54:57 -0300

    qq coisa

--- src/agentes/AgenteAtuador.java
+++ src/agentes/AgenteAtuador.java
@@ -1,7 +1,27 @@
 package agentes;
 
+import gramatica.Centralizador.Absyn.ECollect1;
+import gramatica.Centralizador.Absyn.ECollect2;
+import gramatica.Centralizador.Absyn.EData1;
+import gramatica.Centralizador.Absyn.EData2;
+import gramatica.Centralizador.Absyn.EData3;
+import gramatica.Centralizador.Absyn.EData4;
+
+import java.util.List;
+
+import agentes.AgenteMonitor.InformBilirrubinaBehaviour;
+import agentes.AgenteMonitor.InformHemoglobinaBehaviour;
+import agentes.AgenteMonitor.InformTempBehaviour;
+import agentes.jade.Centralizador.GrammarParserCentralizador;
+import agentes.jade.Centralizador.TarefaCentralizador;
 import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 
@@ -9,30 +29,140 @@ public class AgenteAtuador extends Agent {
 
 	private static final long serialVersionUID = 1L;
 
-	protected void setup() {
-		
-		addBehaviour(new CyclicBehaviour(this) {
+	private ACLMessage ACLmsg;
+	private Boolean isDipironaRunning, isParacetamolRunning = false;
+	private Behaviour checkDipirona, checkParacetamol = null;
 
+	protected void setup() {
+
+		// Regista o monitor no servico de paginas amarelas
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.setName(getAID());
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("monitor");
+		sd.setName(getName());
+		dfd.addServices(sd);
+		try {
+			DFService.register(this, dfd);
+		} catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
+
+		addBehaviour(new CyclicBehaviour(this) {
 			private static final long serialVersionUID = 1L;
 
 			@Override
 			public void action() {
+				// blockingReceive();
+				ACLmsg = receive(MessageTemplate.MatchPerformative(ACLMessage.REQUEST));
 
-				ACLMessage msg = receive(MessageTemplate.MatchPerformative(ACLMessage.REQUEST));
-				if (msg != null) {
-//					System.out.println("Recebi mensagem de " + msg.getSender());
-//					System.out.println("Conteudo : " + msg.getContent());
-					
-					sendMsgtoCentralizador(msg);
+				if (ACLmsg != null) {
+					String mensagem = ACLmsg.getContent().toString();
+					try {
+						TarefaCentralizador tc = GrammarParserCentralizador.getCentralizadorMessageObject(mensagem);
+						if (tc.getAcao() instanceof ECollect1) { // veja na gramatica o que significa ECollect1
+							System.out.print("Atuador: Liberando Remedio");
+							List<Object> dados = tc.getDados();
+							for (Object o : dados) {
+								if (o.equals("")) { // **********************************CHANGE***********************************
+									System.out.print(" Dipirona\n");
+									if (!isDipironaRunning) {
+										checkDipirona = new InformDipironaBehaviour(myAgent, ACLmsg);
+										addBehaviour(checkDipirona);
+										isDipironaRunning = true;
+									} else
+										System.out.println("Monitor " + getName() + " ja esta liberando Dipirona.");
+								} else if (o.equals("")) { // **********************************CHANGE***********************************
+									System.out.print(" Paracetamol\n");
+									if (!isParacetamolRunning) {
+										checkParacetamol = new InformParacetamolBehaviour(myAgent, ACLmsg);
+										addBehaviour(checkParacetamol);
+										isParacetamolRunning = true;
+									} else
+										System.out.println("Monitor " + getName() + " ja esta monitorando Paracetamol.");
+								}
+							}
+						} else if (tc.getAcao() instanceof ECollect2) {
+							System.out.print("Monitor: Parando medicao");
+							List<Object> dados = tc.getDados();
+							for (Object o : dados) {
+								if (o instanceof EData1) { // **********************************CHANGE***********************************
+									System.out.print(" Dipirona\n");
+									if (isDipironaRunning) {
+										removeBehaviour(checkDipirona);
+									} else
+										System.out.println("\nMonitor "	+ getName() + " nao esta monitorando Dipirona.");
+								} else if (o instanceof EData2) {// **********************************CHANGE***********************************
+									System.out.print(" Hemoglobina\n");
+									if (isParacetamolRunning) {
+										removeBehaviour(checkParacetamol);
+									} else
+										System.out.println("\nMonitor " + getName()+ " nao esta monitorando Paracetamol.");
+								}
+							}
+
+						}
+					} catch (Exception e) {
+						System.out.println(e.getMessage());
+					}
 				}
 			}
 		});
 	}
 
-	private void sendMsgtoCentralizador(ACLMessage msg) {
+	
+	// MONITOR informs to Centralizador a given DIPIRONA at each 2s
+	class InformDipironaBehaviour extends TickerBehaviour {
+		private static final long serialVersionUID = 1L;
+		private ACLMessage aclMessage;
+
+		public InformDipironaBehaviour(Agent a, ACLMessage aclMessage) {
+			super(a, 2000);
+			this.aclMessage = aclMessage;
+			System.out.println("Monitor: aclMessage********************************" + aclMessage);
+		}
+
+		@Override
+		protected void onTick() {
+			// int Min = 36;
+			// int Max = 41;
+			// int temperatura = Min + (int) (Math.random() * ((Max - Min) +
+			// 1));
+			String resposta = "Liberando Dipirona";
+			System.out.println("Monitor: msgtoSend para Centralizador  >>>>> "+ resposta);
+			sendMsgtoCentralizador(aclMessage, resposta);
+		}
+
+	}
+
+	// MONITOR informs to Centralizador a given PARACETAMOL at each 2s
+	class InformParacetamolBehaviour extends TickerBehaviour {
+		private static final long serialVersionUID = 1L;
+		private ACLMessage aclMessage;
+
+		public InformParacetamolBehaviour(Agent a, ACLMessage aclMessage) {
+			super(a, 2000);
+			this.aclMessage = aclMessage;
+			System.out.println("Monitor: aclMessage********************************"+ aclMessage);
+		}
+
+		@Override
+		protected void onTick() {
+			// int Min = 36;
+			// int Max = 41;
+			// int temperatura = Min + (int) (Math.random() * ((Max - Min) +
+			// 1));
+			String resposta = "Liberando Paracetamol";
+			System.out.println("Monitor: msgtoSend para Centralizador  >>>>> " + resposta);
+			sendMsgtoCentralizador(aclMessage, resposta);
+		}
+
+	}
+
+	private void sendMsgtoCentralizador(ACLMessage msg, String msgToSend) {
 		ACLMessage replica = msg.createReply();
 		replica.setPerformative(ACLMessage.INFORM);
-		replica.setContent("Foi Aplicado a Droga no paciente");
+		replica.setContent(msgToSend);
 		send(replica);
 
 	}
--- src/agentes/AgenteMonitor.java
+++ src/agentes/AgenteMonitor.java
@@ -20,7 +20,6 @@ import jade.lang.acl.MessageTemplate;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
-import java.util.Random;
 
 import agentes.jade.Centralizador.GrammarParserCentralizador;
 import agentes.jade.Centralizador.TarefaCentralizador;
@@ -30,11 +29,11 @@ public class AgenteMonitor extends Agent {
 	private static final long serialVersionUID = 1L;
 
 	private ACLMessage ACLmsg;
-	private Boolean isTempRunning = false;
-	private Behaviour medeTemperatura = null; 
+	private Boolean isTempRunning, isHemoglobinaRunning, isBilirrubinaRunning, isBloodPressureRunning = false;
+	private Behaviour checkTemperature, checkHemoglobina, checkBilirrubina, checkBloodPressure = null;
 
 	protected void setup() {
-		
+
 		// Regista o monitor no servico de paginas amarelas
 		DFAgentDescription dfd = new DFAgentDescription();
 		dfd.setName(getAID());
@@ -58,119 +57,95 @@ public class AgenteMonitor extends Agent {
 
 				if (ACLmsg != null) {
 					String mensagem = ACLmsg.getContent().toString();
-					try { 
+					try {
 						TarefaCentralizador tc = GrammarParserCentralizador.getCentralizadorMessageObject(mensagem);
 						if (tc.getAcao() instanceof ECollect1) { // veja na gramatica o que significa ECollect1
-							System.out.print("Monitor: Iniciando a medicacao");
+							System.out.print("Monitor: Iniciando a medicao");
 							List<Object> dados = tc.getDados();
-							for (Object o : dados){
-								if (o instanceof EData1) {
+							for (Object o : dados) {
+								if (o instanceof EData1) { // TEMPERATURE
 									System.out.print(" Temperatura\n");
 									if (!isTempRunning) {
-										addBehaviour(new InformTempBehaviour(myAgent,ACLmsg));
+										checkTemperature = new InformTempBehaviour(myAgent, ACLmsg);
+										addBehaviour(checkTemperature);
 										isTempRunning = true;
-									} else System.out.println("Monitor " + getName() + " ja esta monitorando temperatura.");
-								}
-								else if (o instanceof EData2)
+									} else
+										System.out.println("Monitor " + getName() + " ja esta monitorando temperatura.");
+								} else if (o instanceof EData2) { // HEMOGLOBINA
 									System.out.print(" Hemoglobina\n");
-								else if (o instanceof EData3)
-									System.out.print(" bilirrubina\n");
-								else if (o instanceof EData4)
+									if (!isHemoglobinaRunning) {
+										checkHemoglobina = new InformHemoglobinaBehaviour(myAgent, ACLmsg);
+										addBehaviour(checkHemoglobina);
+										isHemoglobinaRunning = true;
+									} else
+										System.out.println("Monitor " + getName() + " ja esta monitorando Hemoglobina.");
+								} else if (o instanceof EData3) { // BILIRRUBINA
+									System.out.print(" Bilirrubina\n");
+									if (!isBilirrubinaRunning) {
+										checkBilirrubina = new InformBilirrubinaBehaviour(myAgent, ACLmsg);
+										addBehaviour(checkBilirrubina);
+										isBilirrubinaRunning = true;
+									} else
+										System.out.println("Monitor " + getName() + " ja esta monitorando Bilirrubina.");
+								} else if (o instanceof EData4) { // PRESSAO ARTERIAL
 									System.out.print(" Pressao Arterial\n");
+									if (!isBloodPressureRunning) {
+										checkBloodPressure = new InformBilirrubinaBehaviour(myAgent, ACLmsg);
+										addBehaviour(checkBloodPressure);
+										isBloodPressureRunning = true;
+									} else
+										System.out.println("Monitor " + getName() + " ja esta monitorando Pressao Arterial.");
+								}
 							}
-						}
-						else if (tc.getAcao() instanceof ECollect2) {
+						} else if (tc.getAcao() instanceof ECollect2) {
 							System.out.print("Monitor: Parando medicao");
 							List<Object> dados = tc.getDados();
-							for (Object o : dados){
+							for (Object o : dados) {
 								if (o instanceof EData1) {
 									System.out.print(" Temperatura\n");
 									if (isTempRunning) {
-										removeBehaviour(medeTemperatura);
-									} else System.out.println("\nMonitor " + getName() + " nao esta monitorando temperatura.");
-								}
-								else if (o instanceof EData2)
+										removeBehaviour(checkTemperature);
+									} else
+										System.out.println("\nMonitor " + getName() + " nao esta monitorando Temperatura.");
+								} else if (o instanceof EData2) {
 									System.out.print(" Hemoglobina\n");
-								else if (o instanceof EData3)
+									if (isHemoglobinaRunning) {
+										removeBehaviour(checkHemoglobina);
+									} else
+										System.out.println("\nMonitor " + getName() + " nao esta monitorando Hemoglobina.");
+								} else if (o instanceof EData3) {
 									System.out.print(" bilirrubina\n");
-								else if (o instanceof EData4)
+									if (isBilirrubinaRunning) {
+										removeBehaviour(checkBilirrubina);
+									} else
+										System.out.println("\nMonitor " + getName() + " nao esta monitorando Bilirrubina.");
+								} else if (o instanceof EData4) {
 									System.out.print(" Pressao Arterial\n");
+									if (isBloodPressureRunning) {
+										removeBehaviour(checkBloodPressure);
+									} else
+										System.out.println("\nMonitor "	+ getName() + " nao esta monitorando Pressao Arterial.");
+								}
 							}
-							
+
 						}/*
-						else if (tc.getAcao() instanceof EApply1) {
-							System.out.print("Liberando medicacao");
-							List<Medicamento> med = tc.getMedicacao();
-							ImprimeMedicacao(med);
-						}
-						else if (tc.getAcao() instanceof EApply2){
-							System.out.print("Cessando liberacao da medicacao");
-							List<Medicamento> med = tc.getMedicacao();
-							ImprimeMedicacao(med);
-							}
-						System.out.println("\n-------------------");
-						*/	
-					} catch (Exception e){
+						 * else if (tc.getAcao() instanceof EApply1) {
+						 * System.out.print("Liberando medicacao");
+						 * List<Medicamento> med = tc.getMedicacao();
+						 * ImprimeMedicacao(med); } else if (tc.getAcao()
+						 * instanceof EApply2){
+						 * System.out.print("Cessando liberacao da medicacao");
+						 * List<Medicamento> med = tc.getMedicacao();
+						 * ImprimeMedicacao(med); }
+						 * System.out.println("\n-------------------");
+						 */
+					} catch (Exception e) {
 						System.out.println(e.getMessage());
 					}
-//					if (ParseGrammar.validateSentence(ACLmsg.getContent().toString())) { // GRAMMAR validation
-//						System.out.println("ParseGramma  TRUE");
-//
-//						if (ACLmsg.getContent().toString().contains(Actions.INICIAR_LEITURA_TEMPERATURA_MONITOR)) {
-//							addBehaviour(new InformTempBehaviour(myAgent,ACLmsg));
-//						}
-//						// OTHERS
-//					} else {
-//						System.out.println("FALSE");
-//					}
 				}
 			}
 		});
 	}
-
-	private void sendMsgtoCentralizador(ACLMessage msg, String msgToSend) {
-		ACLMessage replica = msg.createReply();
-		replica.setPerformative(ACLMessage.INFORM);
-//		currentNumber = defineNumber(currentNumber);
-//
-//		msgtoSend = "A " + action + "16:00: "+ Integer.toString(currentNumber);
-
-		//System.out.println("msg enviada vy monitor " + msgToSend );
-		replica.setContent(msgToSend);
-		send(replica);
-
-	}
-
-	private int randomNumber(int max) {
-		return (int) (Math.random() * max);
-	}
-	
-//	private void generateTempMSG(ACLMessage ACLmsg,int previousDegree){
-//		String 	msgtoSend  ;
-//		int currentDegree = randomNumber(previousDegree);
-//		
-//		msgtoSend =  "A " + "TEMPERATURA"+ " as "+  getCurrentTime() + ": "+ Integer.toString(currentDegree) + "C    " + " |" + Integer.toString(currentDegree) ;  // DEFINIR MELHOR A ACTION(TEMPERATURA) E horario(random)
-//		System.out.println("msgtoSend  >>>>> "  +  msgtoSend);
-//		sendMsgtoCentralizador(ACLmsg, msgtoSend);
-//		
-//	}
-	
-	private String getCurrentTime(){
-	
-    	return new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
- 
-	}
-
-	private void generateDiseasesMSG(ACLMessage ACLmsg,int currentDegree){
-		String 	msgtoSend  ;
-		
-		msgtoSend =  "A " + "Hemoglobina"+ " as "+  getCurrentTime() + ": "+ Integer.toString(randomNumber(currentDegree)) + "g/dL";  // DEFINIR MELHOR A ACTION(hemoglobina) E horario(random)
-		sendMsgtoCentralizador(ACLmsg, msgtoSend);
-		
-	}
-	
-	
-		
 
 	
 	// MONITOR informs to Centralizador a given temperature at each 2s
@@ -181,46 +156,104 @@ public class AgenteMonitor extends Agent {
 		public InformTempBehaviour(Agent a, ACLMessage aclMessage) {
 			super(a, 2000);
 			this.aclMessage = aclMessage;
-			System.out.println("Monitor: aclMessage********************************"+ aclMessage);
+			System.out.println("Monitor: aclMessage********************************" + aclMessage);
 		}
 
 		@Override
 		protected void onTick() {
-			//generateTempMSG(aclMessage, 40);  // get CurrentDegree
-//			int currentDegree = randomNumber(previousDegree);
-//			
-//			msgtoSend =  "A " + "TEMPERATURA"+ " as "+  getCurrentTime() + ": "+ Integer.toString(currentDegree) + "C    " + " |" + Integer.toString(currentDegree) ;  // DEFINIR MELHOR A ACTION(TEMPERATURA) E horario(random)
-//			System.out.println("msgtoSend  >>>>> "  +  msgtoSend);
-//			sendMsgtoCentralizador(ACLmsg, msgtoSend);
-			Random rn = new Random();
 			int Min = 36;
 			int Max = 41;
-			int temperatura = Min + (int)(Math.random() * ((Max - Min) + 1));
+			int temperatura = Min + (int) (Math.random() * ((Max - Min) + 1));
 			String resposta = "Temperatura de " + temperatura + " C as 19 h: 59 m";
-			System.out.println("Monitor: msgtoSend para Centralizador  >>>>> "  +  resposta);
+			System.out.println("Monitor: msgtoSend para Centralizador  >>>>> " + resposta);
 			sendMsgtoCentralizador(aclMessage, resposta);
 		}
 	}
+
 	
-/*	private String defineAction(ACLMessage ACLmsg, String msgReceived) {
+	// MONITOR informs to Centralizador a given Hemoglobina at each 2s
+	class InformHemoglobinaBehaviour extends TickerBehaviour {
+		private static final long serialVersionUID = 1L;
+		private ACLMessage aclMessage;
 
-		if (msgReceived.contains(Actions.INICIAR_LEITURA_TEMPERATURA_MONITOR)) {
-			
-			 generateTempMSG(ACLmsg, 40);  // DEFENI CURRENT DEGREE 
-
-		} else if (msgReceived.contains(Actions.INICIAR_LEITURA_HEMOGRAMA_MONITOR)) {
-			generateDiseasesMSG(ACLmsg, 14);  // DEFENI CURRENT DEGREE 
+		public InformHemoglobinaBehaviour(Agent a, ACLMessage aclMessage) {
+			super(a, 2000);
+			this.aclMessage = aclMessage;
+			System.out.println("Monitor: aclMessage********************************" + aclMessage);
 		}
-		else if (msgReceived.contains(Actions.PARAR_LEITURA_TEMPERATURA_MONITOR)) {}  
-		else if (msgReceived.contains(Actions.PARAR_LEITURA_HEMOGRAMA_MONITOR)) {}  /// OTHRES/// OTHRES
 
-		return currentAction;
+		@Override
+		protected void onTick() {
+			int Min = 36;
+			int Max = 41;
+			int unit = Min + (int) (Math.random() * ((Max - Min) + 1)); //REMOVE this random from all classes
+			String resposta = "Hemoglobina  " + unit + " mg/dL as 19 h: 59 m";
+			System.out.println("Monitor: msgtoSend para Centralizador  >>>>> "+ resposta);
+			sendMsgtoCentralizador(aclMessage, resposta);
+		}
+	}
+
+	
+	// MONITOR informs to Centralizador a given Bilirrubina at each 2s
+	class InformBilirrubinaBehaviour extends TickerBehaviour {
+		private static final long serialVersionUID = 1L;
+		private ACLMessage aclMessage;
+
+		public InformBilirrubinaBehaviour(Agent a, ACLMessage aclMessage) {
+			super(a, 2000);
+			this.aclMessage = aclMessage;
+			System.out.println("Monitor: aclMessage********************************" + aclMessage);
+		}
+
+		@Override
+		protected void onTick() {
+			int Min = 36;
+			int Max = 41;
+			int unit = Min + (int) (Math.random() * ((Max - Min) + 1)); // //REMOVE this random from all classes
+			String resposta = "Bilirrubina  " + unit + " g/dL as 19 h: 59 m";
+			System.out.println("Monitor: msgtoSend para Centralizador  >>>>> " + resposta);
+			sendMsgtoCentralizador(aclMessage, resposta);
+		}
+	}
+
+	
+	// MONITOR informs to Centralizador a given PRESSAO ARTERIAL at each 2s
+	class InformBloodPressureBehaviour extends TickerBehaviour {
+		private static final long serialVersionUID = 1L;
+		private ACLMessage aclMessage;
+
+		public InformBloodPressureBehaviour(Agent a, ACLMessage aclMessage) {
+			super(a, 2000);
+			this.aclMessage = aclMessage;
+			System.out.println("Monitor: aclMessage********************************" + aclMessage);
+		}
+
+		@Override
+		protected void onTick() {
+			int Min = 36;
+			int Max = 41;
+			int unit = Min + (int) (Math.random() * ((Max - Min) + 1)); // ///REMOVE this random from all classes
+			String resposta = "Pressao Arterial  " + unit + " mmHg as 19 h: 59 m";
+			System.out.println("Monitor: msgtoSend para Centralizador  >>>>> " + resposta);
+			sendMsgtoCentralizador(aclMessage, resposta);
+		}
+	}
+
+	private void sendMsgtoCentralizador(ACLMessage msg, String msgToSend) {
+		ACLMessage replica = msg.createReply();
+		replica.setPerformative(ACLMessage.INFORM);
+		replica.setContent(msgToSend);
+		send(replica);
 
 	}
 
-	private int getIndex(String sentence) {
-		return currentDegree;
+	private int randomNumber(int max) {
+		return (int) (Math.random() * max);
+	}
 
-	}*/
+	private String getCurrentTime() {
 
+		return new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance() .getTime());
+
+	}
 }
