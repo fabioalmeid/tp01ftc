8e6bb7529450deabfa85dcda2aec61f51f4bddde
fabioalmeid <fabio_desenv@yahoo.com.br>
2013-09-16 21:31:24 -0300

    updateOutputFile method created

--- src/agentes/jade/Centralizador/AgenteCentralizador.java
+++ src/agentes/jade/Centralizador/AgenteCentralizador.java
@@ -22,6 +22,10 @@ import jade.lang.acl.MessageTemplate;
 import jade.wrapper.AgentContainer;
 import jade.wrapper.StaleProxyException;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 import util.GeradorAleatorioMsg;
@@ -42,7 +46,7 @@ public class AgenteCentralizador extends Agent {
 	private final static int MAX_BILI_BOA = 5;
 	
 	private static final int INTERVALO_REQUISICAO = 8000;
-	private final int AGENTSNUMBER = 1;
+	private final int AGENTSNUMBER = 2;
 	
 	private AtuadorBusyControl atuadorIsBusy[] = new AtuadorBusyControl[AGENTSNUMBER];
 	
@@ -50,6 +54,8 @@ public class AgenteCentralizador extends Agent {
 	private List<AID> atuador;
 	
 	private String monitorName, atuadorName;
+	
+	private File file;
 	
 	/*
 	 * TODO LIST
@@ -68,7 +74,16 @@ public class AgenteCentralizador extends Agent {
 
 		// create agent t1 on the same container of the creator agent
 		AgentContainer container = (AgentContainer) getContainerController();
-
+   
+		// create the file on project directory
+		file =new File("outputData.txt");
+		try {
+			file.createNewFile();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
 		for (int i = 0; i < AGENTSNUMBER; i++) {
 			// create MONITOR
 			monitorName = "monitor" + Integer.toString(i);
@@ -137,6 +152,7 @@ public class AgenteCentralizador extends Agent {
 											temp.append(getReferenceValue("temperatura"));
 											temp.append(". Aplicando remedio.");
 											System.out.println(temp.toString());
+											updateOutputFile(temp.toString(), file);
 											
 											TarefaCentralizador tarefa = new TarefaCentralizador();
 											tarefa.setAcao(new EApply1()); // aplica
@@ -152,6 +168,7 @@ public class AgenteCentralizador extends Agent {
 										temp.append(getLocalName() + ": Temperatura boa, " + String.valueOf(af.getQuantidade1()) + " C.");
 										temp.append(getReferenceValue("temperatura"));
 										System.out.println(temp.toString());
+										updateOutputFile(temp.toString(), file);
 										
 										if (atuadorIsBusy[Integer.parseInt(indexCurrentAgent)].getApplyDipirona()) {
 											TarefaCentralizador tarefa = new TarefaCentralizador();
@@ -176,6 +193,7 @@ public class AgenteCentralizador extends Agent {
 										temp.append(getLocalName() + ": Hemoglobina boa, " + String.valueOf(af.getQuantidade1()) + " mg/dL");
 										temp.append(getReferenceValue("hemoglobina"));
 										System.out.println(temp.toString());
+										updateOutputFile(temp.toString(), file);
 										
 										if (atuadorIsBusy[Integer.parseInt(indexCurrentAgent)].getApplyDipirona()) {
 											TarefaCentralizador tarefa = new TarefaCentralizador();
@@ -198,6 +216,7 @@ public class AgenteCentralizador extends Agent {
 											temp.append(getReferenceValue("hemoglobina"));
 											temp.append(". Aplicando remedio.");
 											System.out.println(temp.toString());
+											updateOutputFile(temp.toString(), file);
 											
 											TarefaCentralizador tarefa = new TarefaCentralizador();
 											tarefa.setAcao(new EApply1());
@@ -213,11 +232,12 @@ public class AgenteCentralizador extends Agent {
 								}
 								if (af.getDado() instanceof EDados1) { 
 									// Decisao de parar medicao de bilirrubina ou continuar medicao e aplicar remedio
-									if (((af.getQuantidade1() >= MIN_BILI_BOA) && (af.getQuantidade1() < MAX_BILI_BOA))) {
+									if (((af.getQuantidade1() >= MIN_BILI_BOA) && (af.getQuantidade1() <= MAX_BILI_BOA))) {
 										StringBuilder temp = new StringBuilder();
 										temp.append(getLocalName() + ": Bilirrubina boa, " + String.valueOf(af.getQuantidade1()) + " g/dL");
 										temp.append(getReferenceValue("bilirrubina"));
 										System.out.println(temp.toString());
+										updateOutputFile(temp.toString(), file);
 										
 										if (atuadorIsBusy[Integer.parseInt(indexCurrentAgent)].getApplyParacetamol()) {
 											TarefaCentralizador tarefa = new TarefaCentralizador();
@@ -243,6 +263,7 @@ public class AgenteCentralizador extends Agent {
 											temp.append(getReferenceValue("bilirrubina"));
 											temp.append(". Aplicando remedio.");
 											System.out.println(temp.toString());
+											updateOutputFile(temp.toString(), file);
 											
 											TarefaCentralizador tarefa = new TarefaCentralizador();
 											tarefa.setAcao(new EApply1());
@@ -265,6 +286,7 @@ public class AgenteCentralizador extends Agent {
 												" Diast -> " + String.valueOf(af.getQuantidade2()));
 										temp.append(getReferenceValue("pressao"));
 										System.out.println(temp.toString());
+										updateOutputFile(temp.toString(), file);
 										
 										if (atuadorIsBusy[Integer.parseInt(indexCurrentAgent)].getApplyParacetamol()) {
 											TarefaCentralizador tarefa = new TarefaCentralizador();
@@ -288,6 +310,7 @@ public class AgenteCentralizador extends Agent {
 											temp.append(getReferenceValue("pressao"));
 											temp.append(". Aplicando remedio.");
 											System.out.println(temp.toString());
+											updateOutputFile(temp.toString(), file);
 											
 											TarefaCentralizador tarefa = new TarefaCentralizador();
 											tarefa.setAcao(new EApply1());
@@ -305,11 +328,11 @@ public class AgenteCentralizador extends Agent {
 						} catch (Exception e) {
 							System.out.println(getLocalName() + ". Erro ao ler resposta do monitor: " + e.getMessage());
 						}
-					} else if (atuador.contains(sender))
-						System.out.println(getLocalName()
-								+ ": Msg recebida do "
-								+ msg.getSender().getLocalName() + " -->"
-								+ msg.getContent());
+					} else if (atuador.contains(sender)){
+						System.out.println(getLocalName() + ": Msg recebida do " + msg.getSender().getLocalName() + " -->" + msg.getContent());
+						updateOutputFile(getLocalName() + ": Msg recebida do " + msg.getSender().getLocalName() + " -->" + msg.getContent(), file);
+					}
+						
 					else {// ERRO
 						System.out.println(getLocalName() + ": "
 								+ sender.getLocalName() + ", "
@@ -341,6 +364,7 @@ public class AgenteCentralizador extends Agent {
 		message.addReceiver(receiver);
 		message.setContent(mensagem);
 		System.out.println(getLocalName() + ": " + receiver.getLocalName() + ", " + message.getContent());
+		updateOutputFile(getLocalName() + ": " + receiver.getLocalName() + ", " + message.getContent(), file);
 		//System.out.println(getLocalName() + "para " + agentName + " : " + mensagem);
 		send(message);
 	}
@@ -362,4 +386,30 @@ public class AgenteCentralizador extends Agent {
 		
 		return result.toString();
 	}
+	
+
+	private void updateOutputFile(String data, File file) {
+		// if file doesnt exists, then create it
+		// if(!file.exists()){
+		// file.createNewFile();
+		// }
+
+		// true = append file
+		FileWriter fileWritter;
+		try {
+			fileWritter = new FileWriter(file.getName(), true);
+			BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
+//			bufferWritter.write(data);
+			bufferWritter.append(data);
+			bufferWritter.newLine();
+			bufferWritter.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+//		System.out.println("Done");
+
+	} 
+	
 }
